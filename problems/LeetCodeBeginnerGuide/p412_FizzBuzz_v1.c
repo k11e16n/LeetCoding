2da24a4e821c4d4348caf92f8a82c364d2e83077
Ken Chiang <k11e16n@gmail.com>
2024-05-29 18:00:41 +0800

    Update LeetCodeBeginnerGuide
    Practice problem 3

---
+++ problems/LeetCodeBeginnerGuide/p412_FizzBuzz_v1.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) {
+    char** returnArray = NULL;
+    returnArray = malloc( n * sizeof(char*) );
+    *returnSize=n; /* I guess this is to tell the caller the number */
+
+    for(int i=0; i<n; i++){
+        returnArray[i] = malloc(9 * sizeof(char));
+        if( (i+1)%3==0 && (i+1)%5==0 ){
+                returnArray[i]="FizzBuzz";
+        } else if( (i+1)%3 == 0 ){
+                returnArray[i]="Fizz";
+        } else if( (i+1)%5 == 0 ){
+                returnArray[i]="Buzz";
+        }else{
+            sprintf(returnArray[i],"%d",i+1);
+        }
+    }
+    return returnArray;
+}

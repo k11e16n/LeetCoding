8e1c4ed5b49cf24ccfe7d34cf81d93dc18a07fcd
Ken Chiang <k11e16n@gmail.com>
2024-06-13 01:47:55 +0800

    Update Explore Card Track Topic: Arrays 101
    Finish section 3: Deleting items from an array

---
+++ problems/Array101/p26_remove-duplicates-from-sorted-array.c
@@ -0,0 +1,9 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int j=0;
+    for(int i=0; i<numsSize; i++){
+        if(nums[i]>nums[j]){
+            nums[++j]=nums[i];
+        }
+    }
+    return j+1;
+}
---
+++ problems/Array101/p27_remove-element.c
@@ -0,0 +1,35 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int i=0, j=numsSize-1, count=0;
+    
+    while(i<j){
+        if(nums[i] == val){
+            while(i<j && nums[j]==val){
+                count++;
+                j--;
+            }
+            nums[i]=nums[j];
+            count++;
+            j--;
+        }
+        i++;
+    }
+    if(i==j && nums[i] == val){
+        nums[i]++;
+        count++;
+    }
+
+    return numsSize-count;
+}
+
+/* A better method on the internet
+int removeElement(int* nums, int numsSize, int val) {
+    int k=0;
+    int i;
+    for (i =0 ; i < numsSize ; i++) {
+        if(nums[i] != val) {
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+}
+*/

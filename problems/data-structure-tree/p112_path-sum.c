63ecc04a374fe85ec6aabdaae090a57c954d0396
Ken Chiang <k11e16n@gmail.com>
2024-07-16 23:35:37 +0800

    Update Explore Card Track Topic: data structure tree
        Update section 2: solve problems recursively
            Update p112_path-sum.c with better method

--- problems/data-structure-tree/p112_path-sum.c
+++ problems/data-structure-tree/p112_path-sum.c
@@ -11,25 +11,15 @@ bool isLeaf(struct TreeNode* node){
     return (node->left == NULL && node->right == NULL)?true:false;
 }
 
-bool hasPathSum_helper(struct TreeNode* root, int targetSum, int currentSum) {
-    if( root == NULL ){
-        return false;
-    }
-    
-    currentSum += root->val;
-
-    if( currentSum == targetSum && isLeaf(root)){
-        return true;
-    }else{
-        return (hasPathSum_helper(root->left, targetSum, currentSum) || 
-                hasPathSum_helper(root->right, targetSum, currentSum));
-    }
-}
 
 bool hasPathSum(struct TreeNode* root, int targetSum) {
     if( root == NULL ){
         return false;
     }
 
-    return hasPathSum_helper(root, targetSum, 0);
+    if( isLeaf (root) ){
+        return (targetSum == root->val);
+    }else{
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
 }

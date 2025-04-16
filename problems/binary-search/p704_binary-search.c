5e3630367472ba83ba143adbf79af2e16664cd29
Ken Chiang <k11e16n@gmail.com>
2025-04-16 02:26:18 -0400

    Update Explore Card Track Topic: binary search
        Add recursive method for #704

--- problems/binary-search/p704_binary-search.c
+++ problems/binary-search/p704_binary-search.c
@@ -1,3 +1,5 @@
+#define METHOD 2
+#if METHOD == 1
 int search(int* nums, int numsSize, int target) {
     int start = 0, end = numsSize-1, check_pos = 0;
     
@@ -14,3 +16,24 @@ int search(int* nums, int numsSize, int target) {
 
     return -1;
 }
+#endif
+
+#if METHOD == 2
+int do_search(int* arr, int left, int right, int target){
+    if(left > right){
+        return -1;
+    }
+    int mid = (left + right)/2;
+    if(target == arr[mid]){
+        return mid;
+    }else if(target > arr[mid]){
+        return do_search(arr, mid+1, right, target);
+    }else{
+        return do_search(arr, left, mid-1, target);
+    }
+}
+
+int search(int* nums, int numsSize, int target) {
+    return do_search(nums, 0, numsSize-1, target);
+}
+#endif
